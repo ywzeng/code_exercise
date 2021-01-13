65cde66ffb913e5b458d636e39e2f5af1a4819ea
ywzeng <ieywzeng@hotmail.com>
2021-01-13 11:07:47 +0800

    Use ring to rotate the linked list

--- leetcode_0061.py
+++ leetcode_0061.py
@@ -14,7 +14,7 @@ class ListNode:
         self.next = next
 
 
-class Solution:
+class Solution_1:
     def rotateRight(self, head: ListNode, k: int) -> ListNode:
         """ Use an additional list to store the replaced Node. """
         if not head:
@@ -43,3 +43,32 @@ class Solution:
         current_node_list[-1].next = None
         return current_node_list[0]
 
+
+class Solution_2:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        """
+        We can modify this Single Linked List to a ring. Namely let the next node of the last node be the head of this original list.
+        Refind the root node.
+        """
+        if not head:
+            return
+        if not head.next:
+            return head
+
+        # Find the last node.
+        node_num = 1
+        root = head
+        while head.next:
+            head = head.next
+            node_num += 1
+        head.next = root
+        # -k is actually the new root.
+        k = node_num - k % node_num
+
+        prior_node = None
+        while k:
+            prior_node = root
+            root = root.next
+            k -= 1
+        prior_node.next = None
+        return root
