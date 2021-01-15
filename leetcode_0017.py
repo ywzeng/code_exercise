cc69e902976022f64b9b878e2246a1d12315c989
ywzeng <ieywzeng@hotmail.com>
2021-01-15 17:56:37 +0800

    Letter Combinations of a Phone Number

--- leetcode_0017.py
+++ leetcode_0017.py
@@ -1,4 +1,25 @@
-class Solution:
+class Solution_1:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        keyboard_dict = {'2': 'abc', '3': 'def', '4': 'ghi',
+                            '5': 'jkl', '6': 'mno', '7': 'pqrs', 
+                            '8': 'tuv', '9': 'wxyz'}
+        
+        def combine_recursion(current_letters: str, remaining_digits: str, result_list: list) -> None:
+            if not remaining_digits:
+                result_list += [current_letters]
+                return
+            else:
+                for letter in keyboard_dict[remaining_digits[0]]:
+                    combine_recursion(current_letters + letter, remaining_digits[1:], result_list)
+        
+        result_list = []
+        combine_recursion('', digits, result_list)
+        return result_list
+
+class Solution_2:
     def __init__(self):
         self.keyboard_dict = {'2': 'abc', '3': 'def', '4': 'ghi',
                                 '5': 'jkl', '6': 'mno', '7': 'pqrs', 
