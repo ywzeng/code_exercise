5e5a368b919652d8a81ded531c6229796ecde096
ywzeng <ieywzeng@hotmail.com>
2021-01-15 11:13:09 +0800

    Palindrome Number

--- leetcode_0009.py
+++ leetcode_0009.py
@@ -1,5 +1,16 @@
-
 class Solution_1:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        original_num = x
+        reversed_num = 0
+        while x > 0:
+            reversed_num = x % 10 + reversed_num * 10
+            x = x // 10
+        return reversed_num == original_num
+
+
+class Solution__2:
     def isPalindrome(self, x: int) -> bool:
         """ Double points """
         if x < 0:
