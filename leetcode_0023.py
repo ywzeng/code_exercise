010d9219064c08719cf0f2b3b50de9efec1e0ba6
ywzeng <ieywzeng@hotmail.com>
2021-01-16 12:08:20 +0800

    Merge k Sorted Lists - Priority-Queue & Divide-Conquer methods

--- leetcode_0023.py
+++ leetcode_0023.py
@@ -3,6 +3,49 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+"""
+    We employ three methods to solve this problem.
+    1. Priority Queue;
+    2. Divide and Conquer;
+    3. Brute Force Merge;
+    In which, the Divide-Conquer method is the fastest, followed by the Priority-Queue method, and the Brute-Force-Merge method is the slowest method.
+    Note that, We need to learn more about the Priority-Queue and the Heap-realted algorithms.
+"""
+
+from queue import PriorityQueue
+
+
+# Define a priority queue item.
+class PQueueItem:
+    def __init__(self, node: ListNode, val: int):
+        self.node = node
+        self.val = val
+
+    def __lt__(self, other_node):
+        return self.val < other_node.val
+
+
+class Solution_priority_queue:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return
+        priority_queue = PriorityQueue()
+        for head_node in lists:
+            if head_node:
+                priority_queue.put(PQueueItem(head_node, head_node.val))
+        
+        head = ListNode(0)      # Define a virtual head.
+        prior_node = head
+        while priority_queue.queue:
+            current_pq_node = priority_queue.get()
+            prior_node.next = current_pq_node.node
+            prior_node = prior_node.next
+            if current_pq_node.node.next:
+                priority_queue.put(PQueueItem(current_pq_node.node.next, current_pq_node.node.next.val))
+        return head.next
+
+
 class Solution_divide_conquer:
     def merge_2_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if not l1 and not l2:
