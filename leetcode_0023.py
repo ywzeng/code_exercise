755679feafba7b83591d838468680e1d50cd0856
ywzeng <ieywzeng@hotmail.com>
2021-01-16 11:18:00 +0800

    Merge k Sorted Lists

--- leetcode_0023.py
+++ leetcode_0023.py
@@ -1,8 +1,49 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution_divide_conquer:
+    def merge_2_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 and not l2:
+            return
+        if l1 and not l2:
+            return l1
+        if not l1 and l2:
+            return l2
+
+        if l1.val <= l2.val:
+            root = l1
+            l1 = l1.next
+        else:
+            root = l2
+            l2 = l2.next
+        current_node = root
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current_node.next = l1
+                l1 = l1.next
+            else:
+                current_node.next = l2
+                l2 = l2.next
+            current_node = current_node.next
+        if l1:
+            current_node.next = l1
+        if l2:
+            current_node.next = l2
+        return root
+
+    def divide_conquer_merge(self, lists: List[ListNode], left: int, right: int) -> ListNode:
+        """ Recursively divide the list into two parts """
+        if left == right:
+            return lists[left]
+        if left > right:
+            return
+        mid = (left + right) // 2
+        return self.merge_2_lists(self.divide_conquer_merge(lists, left, mid), self.divide_conquer_merge(lists, mid+1, right))
+
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        return self.divide_conquer_merge(lists, 0, len(lists)-1)
 
 
 class Solution_stupid:
