1e06643ffc18b976631db11b69c7c98ff3684fbe
ywzeng <ieywzeng@hotmail.com>
2021-01-17 10:45:55 +0800

    Check If It Is a Straight Line

--- leetcode_1232.py
+++ leetcode_1232.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_1:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         if len(coordinates) == 2:
             return True
@@ -11,4 +11,19 @@ class Solution:
             if (coordinates[i][1]-coordinates[i-1][1])*(coordinates[i-1][0]-coordinates[i-2][0]) != (coordinates[i-1][1]-coordinates[i-2][1])*(coordinates[i][0]-coordinates[i-1][0]):
                 print(i)
                 return False
+        return True
+
+
+class Solution__2:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        """ If two points have the same x-coordiante, the corresponding slop is infinite """
+        if len(coordinates) == 2:
+            return True
+        MAX_NUM = float('inf')      # The maximum number in Python.
+        prior_slop = MAX_NUM if coordinates[0][0] == coordinates[1][0] else (coordinates[1][1]-coordinates[0][1]) / (coordinates[1][0]-coordinates[0][0])
+        for i in range(2, len(coordinates)):
+            current_slop = MAX_NUM if coordinates[i][0] == coordinates[i-1][0] else (coordinates[i][1]-coordinates[i-1][1]) / (coordinates[i][0]-coordinates[i-1][0])
+            if current_slop != prior_slop:
+                return False
+            prior_slop = current_slop
         return True
