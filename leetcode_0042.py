6b7a7615b2fcbf2d792a22acadb829899aab7cc1
ywzeng <ieywzeng@hotmail.com>
2021-01-21 20:55:09 +0800

    接雨水

--- leetcode_0042.py
+++ leetcode_0042.py
@@ -1,3 +1,32 @@
+class Solution_DP:
+    def trap(self, height: List[int]) -> int:
+        """
+        DP
+        Use two lists to record the greatest number of corresponding index's two sides.
+        left_height_list[i] = max(left_height_list[i-1], height[i-1])
+        right_height_list[i] = max(right_height_list[i+1], height[i+1])
+        Omit the first and the last number.
+        """
+        if len(height) < 3:
+            return 0
+        left_height_list = [0 for i in range(len(height))]
+        right_height_list = [0 for i in range(len(height))]
+        left_height_list[0] = height[0]
+        right_height_list[-1] = height[-1]
+
+        for i in range(1, len(height)-1):
+            left_height_list[i] = max(left_height_list[i-1], height[i-1])
+        for i in range(len(height)-2, 0, -1):
+            right_height_list[i] = max(right_height_list[i+1], height[i+1])
+        
+        total_water = 0
+        for i in range(1, len(height)-1):
+            min_height = min(left_height_list[i], right_height_list[i])
+            water = min_height-height[i] if min_height > height[i] else 0
+            total_water += water
+        return total_water
+
+
 class Solution_stupid:
     def trap(self, height: List[int]) -> int:
         """ 
