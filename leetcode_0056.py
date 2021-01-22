a27b9cb2615c4a1de3d604b6ddbc0369359047ec
ywzeng <ieywzeng@hotmail.com>
2021-01-22 11:35:18 +0800

    合并区间

--- leetcode_0056.py
+++ leetcode_0056.py
@@ -3,7 +3,7 @@ class Solution:
         intervals = sorted(intervals, key=lambda item: item[0], reverse=False)
         merge_list = [intervals[0]]
         for i in range(1, len(intervals)):
-            if intervals[i][0] >= merge_list[-1][0] and intervals[i][0] <= merge_list[-1][1]:
+            if intervals[i][0] <= merge_list[-1][1]:
                 merge_list[-1] = [merge_list[-1][0], max(intervals[i][1], merge_list[-1][1])]
             else:
                 merge_list += [intervals[i]]
