1016de22800205a285a77f62b5ca65ad298b8aa0
ywzeng <ieywzeng@hotmail.com>
2021-01-22 10:12:12 +0800

    字符串相乘

--- leetcode_0043.py
+++ leetcode_0043.py
@@ -1,6 +1,6 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        temp_result_list = []       # Reocrd the multiple result of each bit.
+        multiple_result = 0       # Cumulatively reocrd the multiple result.
         
         # From the low bit to high bit, namely from right to left.
         for i in range(len(num1)-1, -1, -1):
@@ -8,5 +8,5 @@ class Solution:
             for j in range(len(num2)-1, -1, -1):
                 bit_2 = ord(num2[j]) - ord('0')
                 bit_result = (bit_1 * bit_2) * (10 ** (len(num2)-1-j+len(num1)-1-i))        # Determing the bit level of the multiple result based on the Multiple Vertical Law。
-                temp_result_list += [bit_result]
-        return str(sum(temp_result_list))
+                multiple_result += bit_result
+        return str(multiple_result)
