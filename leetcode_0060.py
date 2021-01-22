30de71556a18b53c259cb4507dc951aaa8dd7111
ywzeng <ieywzeng@hotmail.com>
2021-01-23 00:21:47 +0800

    排列序列

--- leetcode_0060.py
+++ leetcode_0060.py
@@ -1,4 +1,31 @@
-class Solution:
+class Solution_clever:
+    def getPermutation(self, n: int, k: int) -> str:
+        """
+        Divide n! into n partation, each contains (n-1)! sub-permutations.
+        Iteratively do this divide from n to 1.
+        """
+        from math import factorial
+        if n == 1:
+            if k == 1:
+                return str(n)
+            else:
+                return ''
+        k -= 1
+        candidate_element = [i for i in range(1, n+1)]
+        result = []
+        while n > 0:
+            temp_factorial = factorial(n-1)
+            n_temp_index = k // temp_factorial      # Choose the right high bit of the k-th permutation.
+            result += [str(candidate_element[n_temp_index])]
+            # Update the candidate elements and k.
+            candidate_element = candidate_element[:n_temp_index] + candidate_element[n_temp_index+1:]
+            k -= n_temp_index * temp_factorial
+            n -= 1
+
+        return ''.join(result)
+
+
+class Solution_back_track:
     def getPermutation(self, n: int, k: int) -> str:
         from math import factorial
         def back_track(n: int, track_list: list, result_list: list, k: int) -> bool:
