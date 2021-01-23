82896564f1d8411fa07f048f634103a7c995afbc
ywzeng <ieywzeng@hotmail.com>
2021-01-23 19:07:09 +0800

    合并两个有序数组

---
+++ leetcode_0088.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        Because nums1 and nums2 are all ordered lists, if nums2[j] is greater than nums1[i], then nums2[j:] are all greater than nums1[i].
+        """
+        nums1[:] = nums1[:m]        # nums1[:] is the shallow copy.
+        compare_pointer = 0     # Move on the nums1 list.
+        for num in nums2:
+            while compare_pointer < len(nums1) and num >= nums1[compare_pointer]:
+                compare_pointer += 1
+            nums1.insert(compare_pointer, num)
+            compare_pointer += 1
