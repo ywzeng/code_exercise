3e3cf02d9e5f300d3d08fe22e54b18da982267b8
ywzeng <ieywzeng@hotmail.com>
2021-01-23 17:02:55 +0800

    编辑距离

--- leetcode_0072.py
+++ leetcode_0072.py
@@ -1,4 +1,29 @@
-class Solution:
+class Solution_quick:
+    def minDistance(self, word1: str, word2: str) -> int:
+        def reverse_dp(i: int, j: int, dp_dict: dict) -> int:
+            """
+            Recursively calculate the dp value from (i, j) to (0, 0).
+            Use Dict to store the dp value, where key is the tuple (i, j), value is the dp value of (i, j).
+            i represents row index, and j represents column index.
+            """
+            if (i, j) in dp_dict:
+                return dp_dict[(i, j)]
+            if i == -1:
+                return j + 1
+            if j == -1:
+                return i + 1
+
+            if word1[i] == word2[j]:
+                dp_dict[(i, j)] = reverse_dp(i-1, j-1, dp_dict)
+            else:
+                dp_dict[(i, j)] = 1 + min(reverse_dp(i-1, j, dp_dict), reverse_dp(i, j-1, dp_dict), reverse_dp(i-1, j-1, dp_dict))
+            return dp_dict[(i, j)]
+        
+        dp_dict = {}
+        return reverse_dp(len(word1)-1, len(word2)-1, dp_dict)
+
+
+class Solution_slow:
     def minDistance(self, word1: str, word2: str) -> int:
         """
         DP
