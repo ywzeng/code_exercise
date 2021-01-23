fab2108b08792b27a1308f09623920f793e72b55
ywzeng <ieywzeng@hotmail.com>
2021-01-23 20:46:34 +0800

    合并两个有序数组

--- leetcode_0088.py
+++ leetcode_0088.py
@@ -4,10 +4,11 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         Because nums1 and nums2 are all ordered lists, if nums2[j] is greater than nums1[i], then nums2[j:] are all greater than nums1[i].
         """
-        nums1[:] = nums1[:m]        # nums1[:] is the shallow copy.
+        nums1[:] = nums1[:m]        # Left value, nums[:] equals to the enumeration of nums[0], nums[1], ...
         compare_pointer = 0     # Move on the nums1 list.
         for num in nums2:
             while compare_pointer < len(nums1) and num >= nums1[compare_pointer]:
                 compare_pointer += 1
             nums1.insert(compare_pointer, num)
             compare_pointer += 1
+        print(nums1)
