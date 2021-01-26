d1e488850093f11aac94f0b1b1a5f9544b3e67ae
ywzeng <ieywzeng@hotmail.com>
2021-01-26 15:51:42 +0800

    搜索旋转排序数组 II

--- leetcode_0081.py
+++ leetcode_0081.py
@@ -1,4 +1,26 @@
-class Solution:
+class Solution_clever:
+    def search(self, nums: List[int], target: int) -> bool:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return True
+            if nums[left] < nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[left] > nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                left += 1
+        return False
+
+
+class Solution_stupid:
     def search(self, nums: List[int], target: int) -> bool:
         def binary_search(nums: list, left: int, right: int, target: int) -> bool:
             if left > right or (left == right and nums[left] != target):
