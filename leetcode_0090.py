b7af21859efdddae5fcb4860840f269561c63005
ywzeng <ieywzeng@hotmail.com>
2021-01-26 20:00:02 +0800

    子集 II

--- leetcode_0090.py
+++ leetcode_0090.py
@@ -1,3 +1,20 @@
+class Solution_better:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def back_track(nums: int, begin: int, track_list: list, result_list: list) -> None:
+            result_list += [track_list[:]]
+            
+            for i in range(begin, len(nums)):
+                if i > begin and nums[i] == nums[i-1]:
+                    continue
+                track_list += [nums[i]]
+                back_track(nums, i+1, track_list, result_list)
+                track_list.pop()
+        nums.sort()
+        track_list, result_list = [], []
+        back_track(nums, 0, track_list, result_list)
+        return result_list
+
+
 class Solution_Hash_Map:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         from collections import Counter
