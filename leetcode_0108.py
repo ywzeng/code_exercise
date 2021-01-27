742733ad2157afe98a12cd40ee033cd37c4a4ee1
ywzeng <ieywzeng@hotmail.com>
2021-01-27 20:22:32 +0800

    将有序数组转换为二叉搜索树

--- leetcode_0108.py
+++ leetcode_0108.py
@@ -6,6 +6,24 @@
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def build_tree(nums: list, left: int, right: int) -> TreeNode:
+            if left > right:
+                return
+            mid = (left + right) // 2
+            root_node = TreeNode(nums[mid])
+            if left < mid:
+                root_node.left = build_tree(nums, left, mid-1)
+            if mid < right:
+                root_node.right = build_tree(nums, mid+1, right)
+            return root_node
+        
+        if not nums:
+            return
+        return build_tree(nums, 0, len(nums)-1)
+
+
+class Solution_stupid:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
         def build_tree(nums: list, left: int, right: int, root_node: TreeNode) -> None:
             if left > right:
                 return
