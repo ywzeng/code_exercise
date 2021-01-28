9e5dacd77c4a8bd45a6f6f5ee4a701af0c502050
ywzeng <ieywzeng@hotmail.com>
2021-01-28 21:17:03 +0800

    最大矩形

--- leetcode_0085.py
+++ leetcode_0085.py
@@ -5,9 +5,10 @@ class Solution:
             return 0
         max_area = 0
         prior_height_list = [0 for i in range(len(matrix[0]))]
+        matrix = [[int(d) for d in row] for row in matrix]
         for row in range(len(matrix)):
             # Add two sentinels.
-            current_height_list = [0] + [int(matrix[row][i])+prior_height_list[i] if int(matrix[row][i])>0 else 0 for i in range(len(matrix[row]))] + [0]
+            current_height_list = [0] + [matrix[row][i]+prior_height_list[i] if matrix[row][i]>0 else 0 for i in range(len(matrix[row]))] + [0]
             stack = [0]     # The monotonic stack only records the index of each bar.
             for i in range(1, len(current_height_list)):
                 while current_height_list[i] < current_height_list[stack[-1]]:
