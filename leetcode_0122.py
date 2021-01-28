fd4164bd32aaf7db1de133b4c3c6cf489dade260
ywzeng <ieywzeng@hotmail.com>
2021-01-28 20:19:28 +0800

    买卖股票的最佳时机 II

--- leetcode_0122.py
+++ leetcode_0122.py
@@ -1,4 +1,18 @@
-class Solution:
+class Solution_DP:
+    def maxProfit(self, prices: List[int]) -> int:
+        """ DP """
+        # dp[i][0] represents the cash when has no stock, dp[i][1] represents the cash when has stock.
+        dp_list = [[0, 0] for i in range(len(prices))]
+        dp_list[0][1] = -prices[0]
+
+        for i in range(1, len(prices)):
+            dp_list[i][0] = max(dp_list[i-1][0], dp_list[i-1][1]+prices[i])       # no stock, so still not buy or sell the stock.
+            dp_list[i][1] = max(dp_list[i-1][1], dp_list[i-1][0]-prices[i])       # has stock, so the stock is buying before or buying today.
+        return dp_list[-1][0]
+
+
+
+class Solution_Greedy:
     def maxProfit(self, prices: List[int]) -> int:
         """ If today's price is lower than the next's price, we can buy on today and sell on the next day. """
         total_profit = 0
