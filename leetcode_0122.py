95077708c17ccc6bc66082626e74db81563644da
ywzeng <ieywzeng@hotmail.com>
2021-01-28 20:34:31 +0800

    买卖股票的最佳时机 II

--- leetcode_0122.py
+++ leetcode_0122.py
@@ -1,3 +1,16 @@
+class Solution_DP_with_Extra_Space:
+    def maxProfit(self, prices: List[int]) -> int:
+        """ DP without maintaining extra lists, namely the space complexity of O(1). """
+        current_cash = 0
+        current_stock = -prices[0]      # The cash after buying stock.
+
+        for i in range(1, len(prices)):
+            temp_cash = max(current_cash, current_stock+prices[i])      # sell
+            temp_stock = max(current_stock, current_cash-prices[i])        # buy
+            current_cash, current_stock = temp_cash, temp_stock
+        return current_cash
+
+
 class Solution_DP:
     def maxProfit(self, prices: List[int]) -> int:
         """ DP """
@@ -9,7 +22,6 @@ class Solution_DP:
             dp_list[i][0] = max(dp_list[i-1][0], dp_list[i-1][1]+prices[i])       # no stock, so still not buy or sell the stock.
             dp_list[i][1] = max(dp_list[i-1][1], dp_list[i-1][0]-prices[i])       # has stock, so the stock is buying before or buying today.
         return dp_list[-1][0]
-
 
 
 class Solution_Greedy:
