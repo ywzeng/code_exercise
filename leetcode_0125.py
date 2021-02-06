01180476e0c626d6614e15e79f6c435c9f03a267
ywzeng <ieywzeng@hotmail.com>
2021-02-06 17:28:15 +0800

    验证回文串

---
+++ leetcode_0125.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """ Double pointers. """
+        if not s:
+            return True
+        
+        s = s.lower()
+        left_p, right_p = 0, len(s)-1
+        while left_p < right_p:
+            if not s[left_p].isalnum():
+                left_p += 1
+                continue
+            if not s[right_p].isalnum():
+                right_p -= 1
+                continue
+            if s[left_p] != s[right_p] or left_p > right_p:
+                return False
+            left_p += 1
+            right_p -= 1
+        return True
