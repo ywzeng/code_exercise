f4d1518ee59f85cc3b80c81ae424615b2672e8a9
ywzeng <ieywzeng@hotmail.com>
2021-02-06 16:12:14 +0800

    填充每个节点的下一个右侧节点指针

---
+++ leetcode_0116.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        """ Layer Traverse. """
+        if not root:
+            return
+        queue = [root]
+        while queue:
+            temp_queue = queue[:]
+            queue = []
+            for i in range(len(temp_queue)-1):
+                temp_queue[i].next = temp_queue[i+1]
+                if temp_queue[i].left:
+                    queue += [temp_queue[i].left]
+                    queue += [temp_queue[i].right]
+            temp_queue[-1].next = None
+            if temp_queue[-1].left:
+                queue += [temp_queue[-1].left]
+                queue += [temp_queue[-1].right]
+        return root
