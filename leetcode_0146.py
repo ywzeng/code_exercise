b3b923afef587d352cb015b80cf501d28c94c6d0
ywzeng <ieywzeng@hotmail.com>
2021-02-08 10:40:38 +0800

    LRU缓存机制

--- leetcode_0146.py
+++ leetcode_0146.py
@@ -1,4 +1,30 @@
-class LRUCache:
+class LRUCache_OrderedDict:
+    from collections import OrderedDict
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        else:
+            return -1
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+        else:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)
+            self.cache[key] = value
+
+
+class LRUCache_Stupid:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
@@ -29,10 +55,3 @@ class LRUCache:
                 del self.k_v_dict[poped_key]
                 self.k_v_dict[key] = value
                 self.cache += [key]
-
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
