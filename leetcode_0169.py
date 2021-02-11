071710966ab956a634c8e930472fc2401dd9e496
ywzeng <ieywzeng@hotmail.com>
2021-02-12 02:08:02 +0800

    多数元素

--- leetcode_0169.py
+++ leetcode_0169.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_Hash_Table:
     def majorityElement(self, nums: List[int]) -> int:
         from collections import Counter
         num_counter = Counter()
@@ -6,3 +6,9 @@ class Solution:
             num_counter[num] += 1
             if num_counter[num] > len(nums) // 2:
                 return num
+
+            
+class Solution_Sort:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums)//2]
