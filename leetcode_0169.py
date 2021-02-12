3efabedc57a577eb9b1c215ec835f4c985d153d7
ywzeng <ieywzeng@hotmail.com>
2021-02-12 10:38:27 +0800

    多数元素

--- leetcode_0169.py
+++ leetcode_0169.py
@@ -1,3 +1,17 @@
+class Solution_Moore_Voting:
+    def majorityElement(self, nums: List[int]) -> int:
+        mode = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[i] == mode:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    mode = nums[i+1]
+        return mode
+
+    
 class Solution_Hash_Table:
     def majorityElement(self, nums: List[int]) -> int:
         from collections import Counter
