d47737dcef515c54f839ab408a1f7c3ca85eff07
ywzeng <ieywzeng@hotmail.com>
2021-02-13 00:30:36 +0800

    找到所有数组中消失的数字

--- leetcode_0448.py
+++ leetcode_0448.py
@@ -1,5 +1,8 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         for i in nums:
-            nums[i%(len(nums))-1] += len(nums)
+            if i > len(nums):
+                nums[i%(len(nums))-1] += len(nums)
+            else:
+                nums[i-1] += len(nums)
         return [i+1 for i in range(len(nums)) if nums[i] <= len(nums)]
