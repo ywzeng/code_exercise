c0eca07bc5f298c06d4f30e5af571b14c4a6133d
ywzeng <ieywzeng@hotmail.com>
2021-02-12 11:22:18 +0800

    打家劫舍

---
+++ leetcode_0198.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        DP. The interval between two indexes could be greater than 2. Actualy, two step plans: 1 or 2.
+        """
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+
+        dp_list = [0 for i in nums]
+        dp_list[0], dp_list[1] = nums[0], nums[1]
+        for i in range(2, len(nums)):
+            if i > 2:
+                dp_list[i] = nums[i] + max(dp_list[i-2], dp_list[i-3])
+            else:
+                dp_list[i] = nums[i] + dp_list[i-2]
+        return max(dp_list[-1], dp_list[-2])
