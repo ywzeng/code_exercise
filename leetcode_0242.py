3203ac7e5f9cbd516d98955f7f267286ba666fe5
ywzeng <ieywzeng@hotmail.com>
2021-02-14 17:30:51 +0800

    有效的字母异位词

--- leetcode_0242.py
+++ leetcode_0242.py
@@ -1,6 +1,11 @@
-class Solution:
+class Solution_HashMap:
     def isAnagram(self, s: str, t: str) -> bool:
         from collections import Counter
         counter_s = Counter(s)
         counter_t = Counter(t)
         return counter_s == counter_t
+
+class Solution_Sort:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s, t = sorted(list(s)), sorted(list(t))
+        return s == t
